aef3ee5248e2afd6f93b4c89d44fa34a671ffcfc
HrithikSampson <hrithikedwardsampson@gmail.com>
2025-05-11 18:10:11 +0530

    hero page layout

---
+++ src/assets/icons.ts
@@ -0,0 +1,28 @@
+import InstagramLineIcon from "remixicon-react/InstagramLineIcon";
+import FacebookLineIcon from "remixicon-react/FacebookLineIcon";
+import DribbbleLineIcon from "remixicon-react/DribbbleLineIcon";
+import YoutubeLineIcon from "remixicon-react/YoutubeLineIcon";
+import GithubLineIcon from "remixicon-react/GithubLineIcon";
+import React from "react";
+import { JSX } from "react";
+
+import type { RemixiconReactIconComponentType } from "remixicon-react";
+
+export const heroIconComponents: Record<string, RemixiconReactIconComponentType> = {
+  Instagram: InstagramLineIcon,
+  Facebook: FacebookLineIcon,
+  Dribbble: DribbbleLineIcon,
+  Youtube: YoutubeLineIcon,
+  Github: GithubLineIcon,
+};
+
+// AboutMe Icons
+
+import GithubFillIcon from "remixicon-react/GithubFillIcon"
+import Projector2LineIcon from "remixicon-react/Projector2LineIcon"
+import GroupLineIcon from "remixicon-react/GroupLineIcon"
+import AwardFillIcon from "remixicon-react/AwardFillIcon"
+
+export const aboutData = [
+    
+]
